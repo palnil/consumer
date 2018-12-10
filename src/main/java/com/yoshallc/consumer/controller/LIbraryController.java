2f592396355639bfb96b81ea4625c471d22da68c
palnil <palnil@gmail.com>
2018-12-09 20:34:23 -0500

    port change

--- src/main/java/com/yoshallc/consumer/controller/LIbraryController.java
+++ src/main/java/com/yoshallc/consumer/controller/LIbraryController.java
@@ -1,11 +1,10 @@
 package com.yoshallc.consumer.controller;
 
 import com.yoshallc.consumer.controller.request.BookRequest;
+import com.yoshallc.consumer.dto.Book;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.print.Book;
 
 @RestController
 public class LIbraryController {
@@ -13,7 +12,7 @@ public class LIbraryController {
     @PostMapping("/checkout")
     public ResponseEntity<Book> checkoutBook(@RequestBody BookRequest bookRequest){
 
-        Book book = new Book();
+        Book book = new Book(1L,"Shade");
 
         return new ResponseEntity <>(book, HttpStatus.OK);
 
--- src/main/java/com/yoshallc/consumer/dto/Book.java
+++ src/main/java/com/yoshallc/consumer/dto/Book.java
@@ -1,7 +1,15 @@
 package com.yoshallc.consumer.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
 
-
+ private Long id;
+ private String name;
 
 }
